b14ba7dde6575df472a8ce25edc9e1656ea2f03b
Sukhpreet Singh <493_bt19@iiitkalyani.ac.in>
2021-09-21 09:41:49 +0530

    Number of Islands (leetcode 200)

--- BFS/Number of islands.cpp
+++ BFS/Number of islands.cpp
@@ -1,8 +1,8 @@
 //Number of Islands (leetcode 200)
 
-/* We can use DFS to solve this problem.
+/* We can use recursive BFS to solve this problem.
 Algorithm: 
-	1.Search for the '1' in grid if found start DFS from that position
+	1.Search for the '1' in grid if found start BFS from that position
 	2.search in all 4 direction and mark all ones by any character except '0' and '1'.
 	3.keep track of index out of bound.
 	4.Increase the count after calling the DFS.
@@ -10,7 +10,7 @@ Algorithm:
 */
 	
 //Code:	
-void dfs(vector<vector<char>>&grid,int i,int j)
+void bfs(vector<vector<char>>&grid,int i,int j)
     {
     	//checking index out of bound and element is '1' or not
         if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]!='1')
@@ -18,10 +18,10 @@ void dfs(vector<vector<char>>&grid,int i,int j)
         //marking the current '1' as '2' for nto traversing again    
         grid[i][j]='2';
         //calling dfs in all four directions
-        dfs(grid,i+1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j-1);
+        bfs(grid,i+1,j);
+        bfs(grid,i,j+1);
+        bfs(grid,i-1,j);
+        bfs(grid,i,j-1);
         
     }
     int numIslands(vector<vector<char>>& grid) {
@@ -32,7 +32,7 @@ void dfs(vector<vector<char>>&grid,int i,int j)
             {
                 if(grid[i][j]=='1')
                 {
-                    dfs(grid,i,j);
+                    bfs(grid,i,j);
                     ans++;
                 }
             }
